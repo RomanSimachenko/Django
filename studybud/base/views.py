f291f243556decf5c0466843c8b9e049b890dfcf
r0m1mPL <simachenko.roma@gmail.com>
2021-11-18 20:30:18 +0200

    update

---
+++ studybud/base/views.py
@@ -0,0 +1,11 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def room(request):
+    return render(request, 'room.html')
