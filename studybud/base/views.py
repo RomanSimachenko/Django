3b887580cb5310ace08e99ed8d7ea0c1ba51f70c
Roma Simachenko <simachenko.roma@gmail.com>
2021-11-25 23:44:17 +0200

    update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,21 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
-# from django.http import HttpResponse
+from .models import Room
+
+
+# rooms = [
+#     {'id': 1, 'name': "Let's learn python"},
+#     {'id': 2, 'name': "Design with me "},
+#     {'id': 3, 'name': "Frontend developers"},
+# ]
 
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room  = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
+import os
 
-import  os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
