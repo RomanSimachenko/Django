819cec9d878174aef8ac0405bd88aef6885a4f12
Roma Simachenko <simachenko.roma@gmail.com>
2021-11-27 13:50:52 +0200

    update

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -17,7 +17,7 @@ def home(request):
 
 
 def room(request, pk):
-    room  = Room.objects.get(id=pk)
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
