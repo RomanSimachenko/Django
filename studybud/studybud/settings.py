656d7d89282db23386138e26367a1444357b7590
r0m1mPL <simachenko.roma@gmail.com>
2021-12-09 20:05:20 +0200

    update

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-se_#_#x-!jgo(yj82ju=kn8f&(oeb49ifg9kgu-vqi42m_&u8i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
