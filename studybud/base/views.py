e72d5aa6886db61041a3906d0aacfcfa9d462d30
r0m1mPL <simachenko.roma@gmail.com>
2021-12-22 00:37:04 +0200

    Update views.py

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,7 +4,7 @@ from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib .auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
