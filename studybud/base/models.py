983d6d17652fc4f183ddcc29389784c190cee6d3
r0m1mPL <simachenko.roma@gmail.com>
2022-01-06 17:40:27 +0200

    update

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -14,7 +14,8 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(
+        User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -34,3 +35,6 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+
+    class Meta:
+        ordering = ['-updated', '-created']
