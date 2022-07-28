4b01e1b4cd56611d1b8da14e7d856826764eaff3
r0m1mPL <simachenko.roma@gmail.com>
2022-07-28 19:01:48 +0300

    update

--- DjangoDocker/TelegramBot/main.py
+++ DjangoDocker/TelegramBot/main.py
@@ -1,10 +1,20 @@
-import time
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import os
 
 
-def main():
-    while True:
-        time.sleep(1)
+API_TOKEN = os.getenv("API_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 
 if __name__ == "__main__":
-    main()
+    executor.start_polling(dp, skip_updates=True)
